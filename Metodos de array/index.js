18731d18c3fc9fb84035f9b5fb0b5524b9f65c48
caio cesar <last_order@outlook.com.br>
2023-07-30 23:20:18 -0300

    adedado

--- Metodos de array/index.js
+++ Metodos de array/index.js
@@ -60,3 +60,16 @@ numbers.sort(function (a, b) {
 });
 
 console.log(numbers);
+
+let indices = [];
+let array = ["a", "b", "a", "c", "a", "d"];
+let elemento = "a";
+let idx = array.indexOf(elemento);
+while (idx != -1) {
+	indices.push(idx);
+	idx = array.indexOf(elemento, idx + 1);
+}
+console.log(indices);
+
+console.log(lista.includes("goku"));
+console.log(lista.includes("bulma"));
